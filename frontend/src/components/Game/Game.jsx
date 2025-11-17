f20f8dda3c19ca56a899993278223b27b4a7de9c
Relyful <13381270+Relyful@users.noreply.github.com>
2025-11-17 18:33:24 +0100

    Start work on game clickHandler

--- frontend/src/components/Game/Game.jsx
+++ frontend/src/components/Game/Game.jsx
@@ -1,8 +1,12 @@
 import styles from './Game.module.css'
 
+function handleGameAreaClick(e) {
+  console.log(e);
+}
+
 function Game() {
   return (
-    <div className={styles.gameArea}>
+    <div className={styles.gameArea} onClick={handleGameAreaClick}>
 
     </div>
   )
