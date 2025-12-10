5cade60f58d3e8314276414b896c16bc94ba8a7c
Relyful <13381270+Relyful@users.noreply.github.com>
2025-12-10 21:57:54 +0100

    Add form to winDialog

--- frontend/src/components/WinDialog/WinDialog.jsx
+++ frontend/src/components/WinDialog/WinDialog.jsx
@@ -1,7 +1,25 @@
 import styles from './WinDialog.module.css';
+import { useFormStatus } from "react-dom"
+
+function Submit() {
+  const { pending } = useFormStatus();
+  return (
+    <button type='submit' disabled={pending}>
+      {pending ? "Saving..." : "Save to leaderboards"}
+    </button>
+  )
+}
 
 export default function WinDialog() {
-  return <div className={styles.dialogBackdrop}>
-    <div className={styles.dialogContent}>You Win</div>
+  return (
+  <div className={styles.dialogBackdrop}>
+    <div className={styles.dialogContent}>
+      You Win
+      <form>
+      <input type="text" name="username" id="username" />
+      <Submit />
+    </form>
+    </div>    
   </div>
+  )
 }
