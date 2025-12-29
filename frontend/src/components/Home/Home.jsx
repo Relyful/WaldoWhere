cfd6804380b488013ab50346e47d77b88d175442
Relyful <13381270+Relyful@users.noreply.github.com>
2025-12-29 20:30:07 +0100

    Add basic styling

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,10 +1,11 @@
+import style from './Home.module.css';
 import { Link } from "react-router";
 
 export default function Homepage() {
   return (
     <>
-      <h1>Welcome to WaldoWhere</h1>
-      <Link to='/game'>Start</Link>
+      <h1 className={style.heading}>Welcome to WaldoWhere</h1>
+      <Link to='/game' className={style.startButton}>Start</Link>
     </>
   )
 }
