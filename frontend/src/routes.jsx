8b0db8fdd789b309c92f311f9407d492f2437f5b
Relyful <13381270+Relyful@users.noreply.github.com>
2025-12-29 19:57:13 +0100

    Add homepage

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -2,13 +2,15 @@ import Header from "./components/Header/Header";
 import Game from "./components/Game/Game";
 import WinDialog from "./components/WinDialog/WinDialog";
 import Leaderboard from "./components/Leaderboard/Leaderboard";
+import Homepage from "./components/Home/Home";
 
 const routes = [
   {
     path: '/',
     Component: Header ,
     children: [
-      {index: true, Component: Game},
+      {index: true, Component: Homepage},
+      {path: '/game', Component: Game},
       {path: '/test', Component: WinDialog},
       {path: '/leaderboard', Component: Leaderboard}
     ]
