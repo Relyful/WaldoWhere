f39385a3e18c38dbdf7c9d8c99a1b02cb9727ede
Relyful <13381270+Relyful@users.noreply.github.com>
2025-12-30 18:45:36 +0100

    Add color and stling

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -4,8 +4,10 @@ import { Link } from "react-router";
 export default function Homepage() {
   return (
     <>
-      <h1 className={style.heading}>Welcome to WaldoWhere</h1>
-      <Link to='/game' className={style.startButton}>Start</Link>
+      <div className={style.container}>
+        <h1 className={style.heading}>Welcome to WaldoWhere</h1>
+        <Link to='/game' className={style.startButton}>Start</Link>
+      </div>
     </>
   )
 }
