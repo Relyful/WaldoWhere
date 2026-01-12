3ad3b7255a9be3ba665958a3ae318be8938675a8
Relyful <13381270+Relyful@users.noreply.github.com>
2026-01-12 22:19:22 +0100

    Add link to my github

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router";
 import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>Made by Rely</footer>
+    <footer className={styles.footer}>Made by&nbsp;<Link target="_blank" to={'https://github.com/Relyful'}>Rely</Link></footer>
   )
 }
