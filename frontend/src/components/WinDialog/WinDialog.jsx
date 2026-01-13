0ef8f8ce2ad8a49d0d04bac20af959021ec906e6
Relyful <13381270+Relyful@users.noreply.github.com>
2026-01-13 22:15:01 +0100

    style entire dialog

--- frontend/src/components/WinDialog/WinDialog.jsx
+++ frontend/src/components/WinDialog/WinDialog.jsx
@@ -1,20 +1,18 @@
-import { callSaveToLeaderboard } from '../../api/gameApi';
-import styles from './WinDialog.module.css';
+import { callSaveToLeaderboard } from "../../api/gameApi";
+import styles from "./WinDialog.module.css";
 import { useFormStatus } from "react-dom";
 import { Link, useNavigate } from "react-router";
-
 
 function Submit() {
   const { pending } = useFormStatus();
   return (
-    <button type='submit' disabled={pending}>
+    <button type="submit" disabled={pending} className={`${styles.formSubmitButt} ${styles.button}`}>
       {pending ? "Saving..." : "Save to leaderboards"}
     </button>
-  )
+  );
 }
 
 export default function WinDialog({ timer }) {
-
   const minutes = Math.floor(timer / 60);
 
   const seconds = timer % 60;
@@ -23,22 +21,29 @@ export default function WinDialog({ timer }) {
 
   async function handleFormOnSubmit(formData) {
     await callSaveToLeaderboard(formData);
-    navigate('/leaderboard');
+    navigate("/leaderboard");
   }
-  
+
   return (
-  <div className={styles.dialogBackdrop}>
-    <div className={styles.dialogContent}>
-      <h3>Congratulations!</h3>
-      <p>Your time is (mm:ss): {`${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`}</p>
-      <form className={`${styles.winForm}`} action={handleFormOnSubmit}>
-        <label htmlFor="username">Enter your name:</label>
-        <input type="text" name="username" id="username" />
-        <Submit />
-        <Link to='/leaderboard'>Leaderboard</Link>
-        <Link to='/'>Homepage</Link>
-      </form>
-    </div>    
-  </div>
-  )
+    <div className={styles.dialogBackdrop}>
+      <div className={styles.dialogContent}>
+        <h2 className={styles.heading2}>Congratulations!</h2>
+        <p>
+          You found Waldo and friends in (mm:ss):{" "}
+          <span
+            className={styles.biggerFont}
+          >{`${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`}</span>
+        </p>
+        <form className={`${styles.winForm}`} action={handleFormOnSubmit}>
+          <label htmlFor="username">Name:</label>
+          <input type="text" name="username" id="username" className={styles.nameInput} />
+          <Submit />
+        </form>
+          <div className={styles.linkContainer}>
+            <Link to="/leaderboard" className={styles.button}>Leaderboard</Link>
+            <Link to="/" className={styles.button}>Homepage</Link>
+          </div>
+      </div>
+    </div>
+  );
 }
