0893d1c82002a4882e1071dded790fd05929c280
Relyful <13381270+Relyful@users.noreply.github.com>
2026-01-20 21:22:26 +0100

    Update game guess Link

--- frontend/src/api/gameApi.js
+++ frontend/src/api/gameApi.js
@@ -21,7 +21,7 @@ export async function callGameStart(controller) {
 
 export async function callGameGuess(x, y, name, signal, setCorrectGuesses, setNotification) {
   try {
-    const response = await fetch(`${backend}/guess`, {
+    const response = await fetch(`${backend}/game/guess`, {
       signal,
       method: "POST",
       headers: {
