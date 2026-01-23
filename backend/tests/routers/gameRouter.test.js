b1611d8010312a316e296059d0d137507387d003
Relyful <13381270+Relyful@users.noreply.github.com>
2026-01-23 21:05:28 +0100

    Mock PrismaClient to test getGuess

--- backend/tests/routers/gameRouter.test.js
+++ backend/tests/routers/gameRouter.test.js
@@ -1,8 +1,24 @@
-const gameRouter = require('../../routers/gameRouter');
 const request = require('supertest');
 const express = require('express');
+
+const mockFindFirst = jest.fn();
+
+jest.mock('../../generated/prisma/client.ts', () => ({
+  PrismaClient: jest.fn().mockImplementation(() => ({
+    waldoGame: {
+      findFirst: mockFindFirst,
+    },
+  })),
+}));
+
+const { PrismaClient } = require('../../generated/prisma/client.ts');
+const prisma = new PrismaClient();
+
+const gameRouter = require('../../routers/gameRouter');
+
 const app = express();
 
+app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 //mock session
 app.use((req, res, next) => {
@@ -11,19 +27,39 @@ app.use((req, res, next) => {
     regenerate: (callback) => {
       callback(null);
     }, 
-  }; 
+  }
   next();
 });
 
 app.use('/', gameRouter);
+
 
 test('stop timer route works', done => {
   request(app)
     .post('/stop')
     .expect(200, done);
 })
+
 test('start timer route works', done => {
   request(app)
     .post('/start')
     .expect(200, done);
 })
+
+test('guess route works', done => {
+  prisma.waldoGame.findFirst.mockResolvedValue({
+    xStart: 0, xEnd: 10, yStart: 0, yEnd: 10
+  });
+  request(app)
+    .post('/guess')
+    .send({ x: 5, y: 5, character: 'Waldo' })
+    .expect("Content-type", /json/)
+    .expect(200)
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+      expect(res.body.hit).toEqual(true)
+      return done();
+    })
+    })
